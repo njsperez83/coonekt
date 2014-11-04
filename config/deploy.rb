45f4c44a73b629cb5ac4cfc78dd529670985a884
Norris Jay Perez <jay@apptivate.co>
2014-11-04 20:09:07 +0800

    configuring capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,23 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 
 namespace :deploy do
 
-  desc 'Restart application'
+  namespace :assets do
+    task :precompile, :roles => assets_role, :except => { :no_release => true } do
+      run <<-CMD.compact
+        cd -- #{latest_release} &&
+        #{rake} RAILS_ENV=#{rails_env.to_s} #{asset_env} assets:precompile
+      CMD
+    end
+  end
+ 
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_configs do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml"
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+  end
+end
+   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
