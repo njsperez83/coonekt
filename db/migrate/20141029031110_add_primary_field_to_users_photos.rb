fdc9a732a85dd41d9da66e20cd9bfa09ed5671ff
Norris Jay Perez <jay@apptivate.co>
2014-11-05 18:42:45 +0800

    configuring capistrano

--- db/migrate/20141029031110_add_primary_field_to_users_photos.rb
+++
@@ -1,6 +0,0 @@
-class AddPrimaryFieldToUsersPhotos < ActiveRecord::Migration
-  def change
-    add_column :users_photos, :primary, :boolean, default: false
-    remove_index :users_photos, :column => :type
-  end
-end
--- db/migrate/20141029060819_modify_field_to_users_photos.rb
+++
@@ -1,7 +0,0 @@
-class ModifyFieldToUsersPhotos < ActiveRecord::Migration
-  def change
-    remove_column :users_photos, :large
-    remove_column :users_photos, :small
-    add_column :users_photos, :image, :string
-  end
-end
