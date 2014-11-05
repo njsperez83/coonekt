6dbd47441baef02fc70a56fcc0ed4565aee0f532
Norris Jay Perez <jay@apptivate.co>
2014-11-05 16:56:38 +0800

    configuring capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,38 @@
-lock '3.2.1'
+# config valid only for Capistrano 3.1
+lock '3.1.0'
 
 set :application, 'coonekt'
-set :repo_url, 'https://github.com/njsperez83/coonekt.git'
+set :repo_url, 'github.com/njsperez83/coonekt.git'
 
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/deploy/coonekt'
-set :user, "deploy"
-set :use_sudo, true
 
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
 set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-#set :keep_releases, 5
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
+# Default value for keep_releases is 5
+# set :keep_releases, 5
 
 namespace :deploy do
 
@@ -23,7 +44,16 @@ namespace :deploy do
     end
   end
 
-  after :publishing, 'deploy:restart'
+  after :publishing, :restart
   after :finishing, 'deploy:cleanup'
+
+  #after :restart, :clear_cache do
+  #  on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+  #  end
+  #end
 
 end
