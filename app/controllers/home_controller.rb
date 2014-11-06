21370fb786c9044af85930514b05d9f91e02fdd3
Norris Jay Perez <jay@apptivate.co>
2014-11-07 03:32:48 +0800

    Zoho config

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-    ActionMailer::Base.mail(:from => "connect@coonekt.com", :to => "njsperez@icloud.com", :subject => "ZOHO TEST", :body => 'Heeeee').deliver
   end
 end
